722106123097169a041bfe5bade69f646008ad50
Corinne <cwilkin8@gmu.edu>
2019-11-12 15:18:06 -0500

    update models

--- tracker/admin.py
+++ tracker/admin.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 from .models import Museum
 from .models import SensorType
 from .models import Sensor
-from .models import Location
+
 
 admin.site.register(Museum)
 admin.site.register(SensorType)
 admin.site.register(Sensor)
-admin.site.register(Location)
