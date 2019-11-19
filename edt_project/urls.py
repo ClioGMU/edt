cafdf2ba1a3eeb3de672fbdbfee0b0133b4b6030
Corinne <cwilkin8@gmu.edu>
2019-11-19 12:48:15 -0500

    update views

--- edt_project/urls.py
+++ edt_project/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'),
       name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'),
-      name='about'),  
+      name='about'), 
+    path('submission/', TemplateView.as_view(template_name='submission.html'),
+      name='submission'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import DataSubmission 
 
 class HomePageView(TemplateView): 
     template_name = 'home.html'
 
 class AboutPageView(TemplateView): 
     template_name = 'about.html'
+
+class SubmissionView(LoginRequiredMixin, ListView):
+    model = DataSubmission
+    template_name = 'submission.html'
+    login_url = 'login'
+
