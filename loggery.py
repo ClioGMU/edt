fa8c16985238accf8104d09c5f725b0203026a82
Corinne <cwilkin8@gmu.edu>
2019-12-09 15:54:35 -0500

    raspberry pi data collection script

---
+++ loggery.py
@@ -0,0 +1,30 @@
+# to collect environmental data from Raspberry Pi
+
+from sense_hat import SenseHat 
+from datetime import datetime
+from csv import writer
+import time
+import csv
+
+
+sense = SenseHat()
+delay = 60
+
+def get_sense_data():
+    sense_data = []
+
+    sense_data.append(sense.get_temperature())
+    sense_data.append(sense.get_humidity())
+    sense_data. append(datetime.now().isoformat())
+
+    return sense_data
+
+with open('data.csv', 'w', newline='') as f:
+    data_writer = writer(f)
+
+    data_writer.writerow(['temperature', 'humidity', 'datetime'])
+
+    while True:
+        data = get_sense_data()
+        data_writer.writerow(data)
+        time.sleep(delay)
