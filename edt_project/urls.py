7e8ee0432f13def7e34329abf3335d339864cd6e
Corinne <cwilkin8@gmu.edu>
2019-12-17 14:50:03 -0500

    security and css updates

--- edt_project/urls.py
+++ edt_project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('c-panel/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'),
